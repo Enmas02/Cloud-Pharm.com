2e20a8678baff082f7862c770d1fa873d993dc8e
Lexther1703 <62774435+Lexther1703@users.noreply.github.com>
2020-03-27 20:38:04 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE HTML>
+<!--
+	Fractal by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Cloud Pharm</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="content">
+					<h1><a href="#">Cloud Pharm</a></h1>
+					<p>Rapido y Sencillo<br />
+			
+				
+				</div>
+				<div class=""><div class="inner"><img src="images/logo.jpeg" alt="" /></div></div>
+			</header>
+
+		<!-- One -->
+			<section id="one" class="wrapper style2 special">
+				<header class="major">
+					<h2>¿Quienes Somos?<br />
+					</h2>
+				</header>
+				<h2>Cloud Pharm</h2>
+				<strong><p>Somos una empresa encargada de la ventas de medicamentos y suministos medicos.<br/>
+					Cloud Pharm desarrollada por los informaticos (Enmanuel Santos, David Rivas y Lexther Peña)
+				  realizaron este proyecto con el fin de facilitar la compra de medicamentos desde la comodidad de tu casa
+				y con los envios a domicilio.</p></strong>
+
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper">
+				<div class="inner alt">
+					<section class="spotlight">
+						<div class="image"><img src="images/mision.jpg" alt="" /></div>
+						<div class="content">
+							<h3>Mision</h3>
+							<p>Ofrecer a los clientes, productos y servicios de calidad y gran variedad, precios competitivos, asesoría, entrega puntual y servicios post-venta, apoyados en una excelente plataforma humana y tecnológica, garantizando la satisfacción del cliente y la rentabilidad de la empresa.</p>
+						</div>
+					</section>
+					<section class="spotlight">
+						<div class="image"><img src="images/vision.jpg" alt="" /></div>
+						<div class="content">
+							<h3>Vision</h3>
+							<p>Mantener el liderazgo del mercado nacional , mediante productos y servicios de alta calidad, personal competente y tecnología de vanguardia, para agregar valor a nuestros clientes, proveedores, empleados y accionistas.</p>
+						</div>
+					</section>
+					<section class="spotlight">
+						<div class="image"><img src="images/valores.jpg" alt="" /></div>
+						<div class="content">
+							<h3>Valores</h3>
+							<p>Tradición</br>
+							  Compromiso</br>
+							Actitud Proactiva</br>
+							Excelencia en el Servicio </br>
+							Trabajo en Equipo</p>
+						</div>
+					</section>
+					<section class="special">
+						<ul class="icons labeled">
+							<li><span class="icon solid fa fa-star"><span class="label">Servicio Rapido</span></span></li>
+
+							<li><span class="icon solid fa fa-check"><span class="label">Efectividad</span></span></li>
+							<li><span class="icon solid fa-home"><span class="label">Servicio a Domicilio</span></span></li>
+							
+						</ul>
+					</section>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="wrapper style2 special">
+				<header class="major">
+					<h2>Objetivos a Cumplir</h2>
+					<p>-Ser la empresa líder del mercado.<br />
+-Incrementar las ventas.<br />
+-Aumentar los ingresos.<br />
+-Generar mayores utilidades.<br />
+-Obtener una mayor rentabilidad.<br />
+-Lograr una mayor participación en el mercado.<br />
+-Ser una marca líder en el mercado.<br />
+-Ser una marca reconocida en el mercado<br />
+					                  </p>
+				</header>
+				<ul class="actions special">
+					
+					<li><a href="#" class="button">Regresar</a></li>
+				</ul>
+			</section>
+
+		
+		-->
+
+		<!-- Footer -->
+			<footer id="footer">
+				<ul class="icons">
+					<li><a href="https://www.facebook.com/cloudpharm/" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+					<li><a href="https://twitter.com/Cloud_Pharm" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="https://www.instagram.com/cloudpharmacy/?hl=es-la" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+				</ul>
+				<p class="copyright">&copy; Todos los derechos Reservados 2020. <a href="http://localhost/farmacia/index.php">Cloud Pharm</a></p>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
